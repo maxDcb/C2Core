c43e7c7a995e47a7c607cfbb3a698b78bbbcf845
maxdcb <kali@kali.com>
2025-03-04 09:10:04 -0500

    syscall cpp file

---
+++ modules/ModuleCmd/syscall.cpp
@@ -0,0 +1,57 @@
+#include "syscall.hpp"
+
+
+DWORD GlobalHash = 0x0;
+
+
+EXTERN_C DWORD getGlobalHash()
+{
+	return GlobalHash;
+}
+
+
+DWORD SW3_HashSyscall(const char *FunctionName)
+{
+    DWORD Hash = 0x811C9DC5; // FNV offset basis
+    DWORD FNV_prime = 0x01000193; // FNV prime
+
+    int c;
+    while (c = *FunctionName++) 
+	{
+        Hash ^= c;                 // XOR the byte into the lowest byte of the hash
+        Hash *= FNV_prime;          // Multiply by FNV prime
+    }
+
+    return Hash & 0xFFFFFFFF;       // Ensure the result is a 32-bit hash
+}
+
+bool compareEntry(Entry i1, Entry i2) 
+{ 
+    return (i1.getAddress() < i2.getAddress()); 
+} 
+
+SyscallList* SyscallList::singleton_= nullptr;
+
+
+SyscallList *SyscallList::GetInstance()
+{
+    if(singleton_==nullptr){
+        singleton_ = new SyscallList();
+    }
+    return singleton_;
+}
+
+
+EXTERN_C DWORD SW3_GetSyscallNumber(DWORD FunctionHash)
+{
+	SyscallList* singleton = SyscallList::GetInstance();
+	return singleton->getSyscallNumber(FunctionHash);
+}
+
+
+EXTERN_C PVOID SW3_GetSyscallAddress(DWORD FunctionHash)
+{
+	SyscallList* singleton = SyscallList::GetInstance();
+	return singleton->getSyscallAddress(FunctionHash);
+}
+
--- modules/ModuleCmd/syscall.hpp
+++ modules/ModuleCmd/syscall.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <windows.h>
 #include <tlhelp32.h>
 #include "include.hpp"
@@ -6,7 +8,9 @@
 #include <algorithm>
 #include <iostream>
 
+extern DWORD GlobalHash;
 
+EXTERN_C DWORD getGlobalHash();
 DWORD SW3_HashSyscall(PCSTR FunctionName);
 EXTERN_C DWORD SW3_GetSyscallNumber(DWORD FunctionHash);
 EXTERN_C PVOID SW3_GetSyscallAddress(DWORD FunctionHash);
@@ -122,31 +126,6 @@ IN PVOID BaseAddress OPTIONAL,
 OUT PVOID Buffer,
 IN SIZE_T BufferSize,
 OUT PSIZE_T NumberOfBytesRead OPTIONAL);
-
-
-DWORD GlobalHash = 0x0;
-
-
-EXTERN_C DWORD getGlobalHash()
-{
-	return GlobalHash;
-}
-
-
-DWORD SW3_HashSyscall(const char *FunctionName)
-{
-    DWORD Hash = 0x811C9DC5; // FNV offset basis
-    DWORD FNV_prime = 0x01000193; // FNV prime
-
-    int c;
-    while (c = *FunctionName++) 
-	{
-        Hash ^= c;                 // XOR the byte into the lowest byte of the hash
-        Hash *= FNV_prime;          // Multiply by FNV prime
-    }
-
-    return Hash & 0xFFFFFFFF;       // Ensure the result is a 32-bit hash
-}
 
 
 template <typename... Args>
@@ -353,10 +332,7 @@ private:
 };
 
 
-bool compareEntry(Entry i1, Entry i2) 
-{ 
-    return (i1.getAddress() < i2.getAddress()); 
-} 
+bool compareEntry(Entry i1, Entry i2);
 
 
 class SyscallList
@@ -525,30 +501,3 @@ public:
         return NULL;
     }
 };
-
-
-SyscallList* SyscallList::singleton_= nullptr;
-
-
-SyscallList *SyscallList::GetInstance()
-{
-    if(singleton_==nullptr){
-        singleton_ = new SyscallList();
-    }
-    return singleton_;
-}
-
-
-EXTERN_C DWORD SW3_GetSyscallNumber(DWORD FunctionHash)
-{
-	SyscallList* singleton = SyscallList::GetInstance();
-	return singleton->getSyscallNumber(FunctionHash);
-}
-
-
-EXTERN_C PVOID SW3_GetSyscallAddress(DWORD FunctionHash)
-{
-	SyscallList* singleton = SyscallList::GetInstance();
-	return singleton->getSyscallAddress(FunctionHash);
-}
-
