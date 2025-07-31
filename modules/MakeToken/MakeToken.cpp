28061844dcf88303578c824c504c4f1db64e06d9
Maxime dcb <40819564+maxDcb@users.noreply.github.com>
2025-07-31 09:47:04 +0200

    Improve MakeToken error handling

--- modules/MakeToken/MakeToken.cpp
+++ modules/MakeToken/MakeToken.cpp
@@ -25,6 +25,8 @@ using namespace std;
 constexpr std::string_view moduleName = "makeToken";
 constexpr unsigned long long moduleHash = djb2(moduleName);
 
+#define ERROR_INVALID_ARGS 1
+
 
 #ifdef _WIN32
 
@@ -114,21 +116,28 @@ int MakeToken::init(std::vector<std::string> &splitedCmd, C2Message &c2Message)
 
 int MakeToken::process(C2Message &c2Message, C2Message &c2RetMessage)
 {
-	const std::string cmd = c2Message.cmd();
+    const std::string cmd = c2Message.cmd();
 
     std::vector<std::string> splitedList;
     splitList(cmd, ";", splitedList);
+
+    c2RetMessage.set_instruction(c2RetMessage.instruction());
+    c2RetMessage.set_cmd(cmd);
+
+    if(splitedList.size() < 3)
+    {
+        c2RetMessage.set_errorCode(ERROR_INVALID_ARGS);
+        return 0;
+    }
 
     std::string domain=splitedList[0];
     std::string username=splitedList[1];
     std::string password=splitedList[2];
 
-   std::string out = makeToken(username, domain, password);
+    std::string out = makeToken(username, domain, password);
 
-	c2RetMessage.set_instruction(c2RetMessage.instruction());
-	c2RetMessage.set_cmd(cmd);
-	c2RetMessage.set_returnvalue(out);
-	return 0;
+    c2RetMessage.set_returnvalue(out);
+    return 0;
 }
 
 //https://docs.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-logonusera
@@ -168,5 +177,18 @@ std::string MakeToken::makeToken(const std::string& username, const std::string&
 
 #endif
 
-	return result;
+        return result;
+}
+
+int MakeToken::errorCodeToMsg(const C2Message &c2RetMessage, std::string& errorMsg)
+{
+#if defined(BUILD_TEAMSERVER) || defined(BUILD_TESTS)
+    int errorCode = c2RetMessage.errorCode();
+    if(errorCode > 0)
+    {
+        if(errorCode == ERROR_INVALID_ARGS)
+            errorMsg = "Failed: Invalid arguments";
+    }
+#endif
+    return 0;
 }
