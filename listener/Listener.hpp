df32e5ce1bcdde19c8f5fb8dc1271ea881de465b
Maxime dcb <40819564+maxDcb@users.noreply.github.com>
2025-09-02 10:28:51 +0200

    Refine listener session utilities

--- listener/Listener.hpp
+++ listener/Listener.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <iostream>
 #include <chrono>
 #include <queue>
 #include <mutex>
+#include <vector>
 
 #include "CommonCommand.hpp"
 #include "Session.hpp"
@@ -23,22 +23,22 @@ public:
 	Listener(const std::string& param1, const std::string& param2, const std::string& type);
 	virtual ~Listener(){};
 
-	const std::string & getParam1();
-	const std::string & getParam2();
-	const std::string & getType();
-	const std::string & getListenerHash();
-	const std::string & getListenerMetadata()
-	{
-		return m_metadata;
-	}
-	int getNumberOfSession();
+        const std::string & getParam1() const;
+        const std::string & getParam2() const;
+        const std::string & getType() const;
+        const std::string & getListenerHash() const;
+        const std::string & getListenerMetadata() const
+        {
+                return m_metadata;
+        }
+        std::size_t getNumberOfSession() const;
 
 	// Session
 	std::shared_ptr<Session> getSessionPtr(int idxSession);
-	std::shared_ptr<Session> getSessionPtr(std::string& beaconHash, std::string& listenerHash);
-	bool isSessionExist(const std::string& beaconHash, const std::string& listenerHash);
-	bool updateSessionPoofOfLife(std::string& beaconHash, std::string& lastProofOfLife);
-	bool markSessionKilled(std::string& beaconhash);
+        std::shared_ptr<Session> getSessionPtr(const std::string& beaconHash, const std::string& listenerHash);
+        bool isSessionExist(const std::string& beaconHash, const std::string& listenerHash);
+        bool updateSessionProofOfLife(const std::string& beaconHash, const std::string& lastProofOfLife);
+        bool markSessionKilled(const std::string& beaconhash);
 
 	// Session Listener
 	bool addSessionListener(const std::string& beaconHash, const std::string& listenerHash, const std::string& type, const std::string& param1, const std::string& param2);
@@ -48,13 +48,13 @@ public:
 	// Task & Task Result
 	void queueTask(const std::string& beaconHash, const C2Message& c2Message);
 	bool addTask(const C2Message& task, const std::string& beaconHash);
-	C2Message getTask(std::string& beaconHash);
-	bool addTaskResult(const C2Message& taskResult, std::string& beaconHash);
-	C2Message getTaskResult(const std::string& beaconHash);
+        C2Message getTask(const std::string& beaconHash);
+        bool addTaskResult(const C2Message& taskResult, const std::string& beaconHash);
+        C2Message getTaskResult(const std::string& beaconHash);
 
 	// SocksSession
-	bool isSocksSessionExist(std::string& beaconHash, std::string& listenerHash);
-	bool addSocksTaskResult(const C2Message& taskResult, std::string& beaconHash);
+        bool isSocksSessionExist(const std::string& beaconHash, const std::string& listenerHash);
+        bool addSocksTaskResult(const C2Message& taskResult, const std::string& beaconHash);
 	C2Message getSocksTaskResult(const std::string& beaconHash);
 
 	// set the listener as primary (meaning launch from the teamserver)
