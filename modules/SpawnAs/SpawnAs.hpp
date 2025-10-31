0733929f5b404bb7e0ef066561bfeea436252097
Maxime dcb <40819564+maxDcb@users.noreply.github.com>
2025-10-31 08:06:25 -0700

    Refactor SpawnAs parameter packing and error handling

--- modules/SpawnAs/SpawnAs.hpp
+++ modules/SpawnAs/SpawnAs.hpp
@@ -2,6 +2,11 @@
 
 #include "ModuleCmd.hpp"
 
+#ifndef _WIN32
+constexpr int LOGON32_LOGON_INTERACTIVE = 2;
+constexpr int LOGON32_LOGON_NEW_CREDENTIALS = 9;
+#endif
+
 
 class SpawnAs : public ModuleCmd
 {
@@ -14,12 +19,24 @@ public:
 
     int init(std::vector<std::string>& splitedCmd, C2Message& c2Message);
     int process(C2Message& c2Message, C2Message& c2RetMessage);
-    int osCompatibility() 
+    int osCompatibility()
     {
         return OS_WINDOWS;
     }
 
+    int errorCodeToMsg(const C2Message& c2RetMessage, std::string& errorMsg) override;
+
 private:
+
+    struct Options
+    {
+        int logonType = LOGON32_LOGON_INTERACTIVE;
+        bool loadProfile = true;
+        bool showWindow = false;
+    };
+
+    std::string packParameters(const Options& options) const;
+    Options unpackParameters(const std::string& data) const;
 
 };
 
