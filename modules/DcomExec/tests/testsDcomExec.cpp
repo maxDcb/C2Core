cd6f9fb4b5fab7712c07aa30aafe4abed95ea926
Maxime dcb <40819564+maxDcb@users.noreply.github.com>
2025-10-31 09:32:50 +0100

    DCom not their yet

--- modules/DcomExec/tests/testsDcomExec.cpp
+++ modules/DcomExec/tests/testsDcomExec.cpp
@@ -8,7 +8,7 @@ int main()
 {
     {
         std::unique_ptr<DcomExec> module = std::make_unique<DcomExec>();
-        std::vector<std::string> cmd = {"dcomExec", "-h", "localhost", "-c", "cmd.exe", "-a", "\"/c", "echo", "ran", ">", "C:\\Users\\vuln\\Desktop\\dcom_test1.txt\""};
+        std::vector<std::string> cmd = {"dcomExec", "-c", "cmd.exe", "-a", "\"/c", "echo", "ran", ">", "C:\\Users\\vuln10\\Desktop\\dcom_test1.txt\""};
         C2Message message;
         C2Message ret;
 
@@ -23,7 +23,7 @@ int main()
     }
     {
         std::unique_ptr<DcomExec> module = std::make_unique<DcomExec>();
-        std::vector<std::string> cmd = {"dcomExec", "-h", "localhost", "-c", "cmd.exe", "-a", "/c calc.exe"};
+        std::vector<std::string> cmd = {"dcomExec", "-u", ".\\root", "-p", "root", "-c", "cmd.exe", "-a", "\"/c", "echo", "ran", ">", "C:\\Users\\root\\Desktop\\dcom_test2.txt\""};
         C2Message message;
         C2Message ret;
 
@@ -38,7 +38,7 @@ int main()
     }
     {
         std::unique_ptr<DcomExec> module = std::make_unique<DcomExec>();
-        std::vector<std::string> cmd = {"dcomExec", "-h", "localhost", "-u", "root", "-p", "root", "-c", "cmd.exe", "-a", "\"/c", "echo", "ran", ">", "C:\\Users\\vuln\\Desktop\\dcom_test2.txt\""};
+        std::vector<std::string> cmd = {"dcomExec", "-u", ".\\root", "-p", "root", "-c", "cmd.exe", "-a", "\"/c", "echo", "ran", ">", "C:\\Users\\root\\Desktop\\dcom_test2.txt\""};
         C2Message message;
         C2Message ret;
 
@@ -53,7 +53,67 @@ int main()
     }
     {
         std::unique_ptr<DcomExec> module = std::make_unique<DcomExec>();
-        std::vector<std::string> cmd = {"dcomExec", "-h", "localhost", "-u", "root", "-p", "toor", "-c", "cmd.exe", "-a", "\"/c", "echo", "ran", ">", "C:\\Users\\vuln\\Desktop\\dcom_test2.txt\""};
+        std::vector<std::string> cmd = {"dcomExec", "-h", "192.168.122.177", "-c", "cmd.exe", "-a", "\"/c", "echo", "ran", ">", "C:\\Users\\root\\Desktop\\dcom_test1.txt\""};
+        C2Message message;
+        C2Message ret;
+
+        module->init(cmd, message);
+        module->process(message, ret);
+
+        std::string err;
+        module->errorCodeToMsg(ret, err);
+
+        std::cout << ret.returnvalue() << std::endl;
+        std::cerr << err << std::endl;
+    }
+    {
+        std::unique_ptr<DcomExec> module = std::make_unique<DcomExec>();
+        std::vector<std::string> cmd = {"dcomExec", "-h", "192.168.122.177", "-c", "cmd.exe", "-a", "/c calc.exe"};
+        C2Message message;
+        C2Message ret;
+
+        module->init(cmd, message);
+        module->process(message, ret);
+
+        std::string err;
+        module->errorCodeToMsg(ret, err);
+
+        std::cout << ret.returnvalue() << std::endl;
+        std::cerr << err << std::endl;
+    }
+    {
+        std::unique_ptr<DcomExec> module = std::make_unique<DcomExec>();
+        std::vector<std::string> cmd = {"dcomExec", "-h", "192.168.122.177", "-u", ".\\root", "-p", "root", "-c", "cmd.exe", "-a", "\"/c", "echo", "ran", ">", "C:\\Users\\root\\Desktop\\dcom_test666.txt\""};
+        C2Message message;
+        C2Message ret;
+
+        module->init(cmd, message);
+        module->process(message, ret);
+
+        std::string err;
+        module->errorCodeToMsg(ret, err);
+
+        std::cout << ret.returnvalue() << std::endl;
+        std::cerr << err << std::endl;
+    }
+    {
+        std::unique_ptr<DcomExec> module = std::make_unique<DcomExec>();
+        std::vector<std::string> cmd = {"dcomExec", "-h", "192.168.122.177", "-u", "root", "-p", "toor", "-c", "cmd.exe", "-a", "\"/c", "echo", "ran", ">", "C:\\Users\\root\\Desktop\\dcom_test2.txt\""};
+        C2Message message;
+        C2Message ret;
+
+        module->init(cmd, message);
+        module->process(message, ret);
+
+        std::string err;
+        module->errorCodeToMsg(ret, err);
+
+        std::cout << ret.returnvalue() << std::endl;
+        std::cerr << err << std::endl;
+    }
+    {
+        std::unique_ptr<DcomExec> module = std::make_unique<DcomExec>();
+        std::vector<std::string> cmd = {"dcomExec", "-h", "192.168.122.59", "-u", ".\\root", "-p", "root", "-c", "cmd.exe", "-a", "\"/c", "echo", "ran", ">", "C:\\Users\\root\\Desktop\\dcom_test2.txt\""};
         C2Message message;
         C2Message ret;
 
